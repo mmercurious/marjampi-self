c46de17abed0ce33dbcd2e088cbc1578d2022155
Marja Ahonen <marja.e.ahonen@gmail.com>
2017-11-29 14:09:46 +0200

    app -> frontpage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import FrontPage from './frontPage/components/FrontPage';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<FrontPage />, document.getElementById('root'));
 registerServiceWorker();
