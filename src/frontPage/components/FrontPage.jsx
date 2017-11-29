eb751f97b129636afdb84bc9791cb529b0eb3403
Marja Ahonen <marja.e.ahonen@gmail.com>
2017-11-29 14:10:17 +0200

    files for front page view

---
+++ src/frontPage/components/FrontPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Header from '../../general/components/Header';
+
+class FrontPage extends React.Component {
+  /*
+    figure out stateless components
+    figure out autocomplete
+  */
+  render() {
+    return (
+      <div>
+        <Header />
+
+        <div>
+          <h2>Marjampi</h2>
+          <h1>Welcome fellas</h1>
+        </div>
+
+        <div>
+          Jea
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default FrontPage;
