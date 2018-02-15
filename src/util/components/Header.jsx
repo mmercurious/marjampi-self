481e298d392f68043da450370839549e70ebad7d
Marja Ahonen <marja.e.ahonen@gmail.com>
2018-02-15 12:30:27 +0200

    Changed Header into stateless component, modified styles

--- src/util/components/Header.jsx
+++ src/util/components/Header.jsx
@@ -4,24 +4,25 @@ import { Nav, NavItem, NavLink } from 'reactstrap';
 import '../css/header.min.css';
 import strings from '../../assets/content/strings.json';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="header__container">
-        <Nav>
-          <NavItem>
-            <NavLink href="#">Index</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Projects</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">{strings.nav.tech}</NavLink>
-          </NavItem>
-        </Nav>
-      </div>
-    );
-  }
-};
+/*
+  TODO: Routing and links
+*/
+
+const Header = () => (
+  <div className="header__container">
+    <Nav>
+      <NavItem>
+        <NavLink href="#">{strings.nav.index}</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">{strings.nav.projects}</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">{strings.nav.tech}</NavLink>
+      </NavItem>
+    </Nav>
+  </div>
+);
+
 
 export default Header;
