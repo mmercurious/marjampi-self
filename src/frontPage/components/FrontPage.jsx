3c45c13f382a25078c8ca9d0fa02c3c395fd2657
Marja Ahonen <marja.e.ahonen@gmail.com>
2018-02-16 11:58:05 +0200

    Added Projects temporarily to front page

--- src/frontPage/components/FrontPage.jsx
+++ src/frontPage/components/FrontPage.jsx
@@ -5,12 +5,18 @@ import Footer from '../../util/components/Footer';
 import FrontPageTitle from './FrontPageTitle';
 import Header from '../../util/components/Header';
 
+import Projects from '../../projects/components/Projects';
+
+/*
+  TODO: Remove Projects from here, was here just for a quick check-up
+*/
 
 const FrontPage = () => (
   <div>
     <Header />
     <FrontPageTitle />
     <Bio />
+    <Projects />
     <Footer />
   </div>
 );
